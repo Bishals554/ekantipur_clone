d2d94acc42a6097251327227381e6ad89b18ad30
bishals554 <bishals554@gmail.com>
2024-07-09 22:20:49 +0545

    added other page

---
+++ lib/screen/latest_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LatestPage extends StatelessWidget {
+  const LatestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Latest Page'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/shorts_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShortsPage extends StatelessWidget {
+  const ShortsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Shorts Page'),
+      ),
+    );
+  }
+}
